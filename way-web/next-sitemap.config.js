cb6f74662c314b30d4ae7cc6e0087fa693ad9259
Rauf221 <Rauf280604@gmail.com>
2025-09-12 14:07:03 +0400

    sitemap

--- way-web/next-sitemap.config.js
+++ way-web/next-sitemap.config.js
@@ -1,5 +1,31 @@
 /** @type {import('next-sitemap').IConfig} */
+const fs = require('fs');
+const path = require('path');
+
+
+const getBlogSlugs = () => {
+
+  return [
+    'historical-facts-of-the-atropatene-state',
+    'historical-research-and-future-perspectives',
+    'the-historical-mission-of-the-young-ambassadors',
+    'history-in-the-context-of-international-relations',
+    'the-fundamental-structure-of-azerbaijani-statehood',
+  ];
+};
+
 module.exports = {
-  siteUrl: 'https://WORLDAZEYOUTH.AZ',
-  generateRobotsTxt: true, // (opsional)
-}
+  siteUrl: 'https://worldazeyouth.az',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  
+ 
+  async additionalPaths(config) {
+    const slugs = getBlogSlugs();
+
+    return slugs.map(slug => ({
+      loc: `/blogs/${slug}`,       
+      lastmod: new Date().toISOString(), 
+    }));
+  },
+};
